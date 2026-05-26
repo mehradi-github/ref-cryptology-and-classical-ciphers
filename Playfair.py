9dc3bb7b4109151b04df36a96787c26987742fe5
Mehradi <mehradi.info@gmail.com>
2026-05-26 18:33:16 +0200

    Adding method decipher(self, ciphertext)

--- Playfair.py
+++ Playfair.py
@@ -57,7 +57,7 @@ class PlayfairCipher:
                     return i, j
         return None
     
-    def encrypt_pair(self, a, b):        
+    def encipher_pair(self, a, b):        
         row1, col1 = self.find_position(a)
         row2, col2 = self.find_position(b)
         
@@ -73,11 +73,43 @@ class PlayfairCipher:
         ciphertext = []
         
         for pair in pairs:
-            encrypted_pair = self.encrypt_pair(pair[0], pair[1])
-            ciphertext.append(encrypted_pair)
+            enciphered_pair = self.encipher_pair(pair[0], pair[1])
+            ciphertext.append(enciphered_pair)
         
         return ''.join(ciphertext)    
+    
+    
+    def decipher_pair(self, a, b):
+        row1, col1 = self.find_position(a)
+        row2, col2 = self.find_position(b)
         
+        if row1 == row2:
+            return (self.matrix[row1][(col1 - 1) % 5] + self.matrix[row2][(col2 - 1) % 5])
+        
+        elif col1 == col2:
+            return (self.matrix[(row1 - 1) % 5][col1] + self.matrix[(row2 - 1) % 5][col2])
+        
+        else:
+            return (self.matrix[row1][col2] + self.matrix[row2][col1])
+            
+                
+    def decipher(self, ciphertext):
+        ciphertext = ciphertext.upper().replace(' ', '')
+        
+        if len(ciphertext) % 2 != 0:
+            ciphertext += 'X'
+        
+        plaintext = []
+        for i in range(0, len(ciphertext), 2):
+            deciphered_pair = self.decipher_pair(ciphertext[i], ciphertext[i+1])
+            plaintext.append(deciphered_pair)
+            
+        result = ''.join(plaintext)        
+        if result.endswith('X'):
+            result = result[:-1]
+        
+        return result
+    
         
 def main():
     key = "PASSWORD"
