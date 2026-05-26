bfbf76d458e8604100ce1a806586c6ffd9cf3d9e
Mehradi <mehradi.info@gmail.com>
2026-05-26 17:35:19 +0200

    adding Playfair.py

---
+++ Playfair.py
@@ -0,0 +1,13 @@
+
+class PlayfairCipher:
+    def __init__(self, key):
+        self.key = key.upper()
+        
+        
+        
+def main():
+    key = "PASSWORD"
+
+
+if __name__ == "__main__":
+    main()
