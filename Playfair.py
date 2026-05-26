91afc45fc0d670023f26a1ccc3463ba80f1db2c8
Mehradi <mehradi.info@gmail.com>
2026-05-26 17:45:10 +0200

    Adding method create_matrix(self)

--- Playfair.py
+++ Playfair.py
@@ -1,7 +1,28 @@
 
 class PlayfairCipher:
     def __init__(self, key):
+        self.alphabet = 'ABCDEFGHIKLMNOPQRSTUVWXYZ' 
         self.key = key.upper()
+        self.matrix = []
+        self.create_matrix()
+        
+        
+    def create_matrix(self):
+        combined = []
+        
+        for char in self.key:
+            if char not in combined and char != 'J':
+                combined.append(char)
+            elif char == 'J' and 'I' not in combined:
+                combined.append('I')        
+         
+        
+        for char in self.alphabet:
+            if char not in combined:
+                combined.append(char)
+        
+        self.matrix = [combined[i:i+5] for i in range(0, 25, 5)]
+        
         
         
         
