2fe200a77b5912f984b8c5bfe5bc782028ad4585
Mehradi <mehradi.info@gmail.com>
2026-05-26 19:16:31 +0200

    Adding method encipher in class RailFenceCipher

--- RailFence.py
+++ RailFence.py
@@ -1,8 +1,29 @@
 
 class RailFenceCipher:
-    def __init__(self, key):
-        pass
+    def __init__(self, rails):
+        self.rails = rails
 
+    def encipher(self, plaintext):
+        plaintext = plaintext.replace(" ", "")
+        
+        fence = [[] for _ in range(self.rails)]
+        
+        rail = 0
+        direction = 1  
+        
+        for char in plaintext:
+            fence[rail].append(char)            
+            if rail == 0:
+                direction = 1
+            elif rail == self.rails - 1:
+                direction = -1
+            
+            rail += direction
+        
+        ciphertext = ''.join([''.join(rail) for rail in fence])
+        
+        return ciphertext
+    
 def main():
     cipher = RailFenceCipher(4)
     
