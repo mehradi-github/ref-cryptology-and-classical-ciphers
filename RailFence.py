ae16e67310195c52a0431a7212902a53cf51306c
Mehradi <mehradi.info@gmail.com>
2026-05-26 20:13:05 +0200

    Adding method decipher in calss RailFenceCipher

--- RailFence.py
+++ RailFence.py
@@ -6,8 +6,7 @@ class RailFenceCipher:
     def encipher(self, plaintext):
         plaintext = plaintext.replace(" ", "")
         
-        fence = [[] for _ in range(self.rails)]
-        
+        fence = [[] for _ in range(self.rails)]        
         rail = 0
         direction = 1  
         
@@ -23,6 +22,48 @@ class RailFenceCipher:
         ciphertext = ''.join([''.join(rail) for rail in fence])
         
         return ciphertext
+    
+    def decipher(self, ciphertext):
+        fence = [[] for _ in range(self.rails)]        
+        rail = 0
+        direction = 1
+        
+        positions = []
+        for i in range(len(ciphertext)):
+            positions.append(rail)            
+            if rail == 0:
+                direction = 1
+            elif rail == self.rails - 1:
+                direction = -1            
+            rail += direction
+        
+        rail_counts = [0] * self.rails
+        for pos in positions:
+            rail_counts[pos] += 1
+        
+        index = 0
+        for i in range(self.rails):
+            for j in range(rail_counts[i]):
+                fence[i].append(ciphertext[index])
+                index += 1
+        
+        plaintext = []
+        rail = 0
+        direction = 1
+        pointers = [0] * self.rails
+        
+        for i in range(len(ciphertext)):
+            plaintext.append(fence[rail][pointers[rail]])
+            pointers[rail] += 1
+            
+            if rail == 0:
+                direction = 1
+            elif rail == self.rails - 1:
+                direction = -1
+            
+            rail += direction
+        
+        return ''.join(plaintext)
     
 def main():
     cipher = RailFenceCipher(4)
